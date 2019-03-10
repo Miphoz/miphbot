290903178808d01ee24f7148207056394df0b776
Miphoz <48419635+Miphoz@users.noreply.github.com>
2019-03-10 22:59:24 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord - require(discord.js);
+
+var bot - new Discord.Client(
+
+    bot.on(ready, function() {
+bot.user.setgame(MiphBot, !help);
+console.log(Le bot a bien ete connecte);
+    });
+    bot.login(6tEOKTlrNURdOKcdylDhSfl9k95jOrlm);
